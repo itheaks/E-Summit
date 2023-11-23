0b1fe5d80b05dc4c7e35d94c5cbd87762b00d1e9
aks <aksmlibts@gmail.com>
2023-11-23 22:45:08 +0530

    inital commit

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -8,37 +8,34 @@ const Cards = () => {
     <div className='w-full py-[10rem] px-4 bg-white'>
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
           <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-              <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Single} alt="/" />
-              <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
-              <p className='text-center text-4xl font-bold'>$149</p>
+              <h2 className='text-2xl font-bold text-center py-8'>Event 1</h2>
+              <p className='text-center text-4xl font-bold'>name...</p>
               <div className='text-center font-medium'>
-                  <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-                  <p className='py-2 border-b mx-8'>1 Granted User</p>
-                  <p className='py-2 border-b mx-8'>Send up to 2 GB</p>
+                  <p className='py-2 border-b mx-8 mt-8'>desc1</p>
+                  <p className='py-2 border-b mx-8'>desc2</p>
+                  <p className='py-2 border-b mx-8'>desc3</p>
               </div>
-              <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
+              <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Register Yourself</button>
           </div>
           <div className='w-full shadow-xl bg-gray-100 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300'>
-              <img className='w-20 mx-auto mt-[-3rem] bg-transparent' src={Double} alt="/" />
-              <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
-              <p className='text-center text-4xl font-bold'>$149</p>
+              <h2 className='text-2xl font-bold text-center py-8'>Events 2</h2>
+              <p className='text-center text-4xl font-bold'>name...</p>
               <div className='text-center font-medium'>
-                  <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-                  <p className='py-2 border-b mx-8'>1 Granted User</p>
-                  <p className='py-2 border-b mx-8'>Send up to 2 GB</p>
+                  <p className='py-2 border-b mx-8 mt-8'>desc1</p>
+                  <p className='py-2 border-b mx-8'>desc2</p>
+                  <p className='py-2 border-b mx-8'>desc3</p>
               </div>
-              <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
+              <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Register Yourself</button>
           </div>
           <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-              <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Triple} alt="/" />
-              <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
-              <p className='text-center text-4xl font-bold'>$149</p>
+              <h2 className='text-2xl font-bold text-center py-8'>name...</h2>
+              <p className='text-center text-4xl font-bold'>name...</p>
               <div className='text-center font-medium'>
-                  <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-                  <p className='py-2 border-b mx-8'>1 Granted User</p>
-                  <p className='py-2 border-b mx-8'>Send up to 2 GB</p>
+                  <p className='py-2 border-b mx-8 mt-8'>desc1</p>
+                  <p className='py-2 border-b mx-8'>desc2</p>
+                  <p className='py-2 border-b mx-8'>desc3</p>
               </div>
-              <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
+              <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Register Yourself</button>
           </div>
       </div>
     </div>
