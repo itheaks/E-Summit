c1508f5f1667198d4cf285f43c7c6ef42c0d0b72
aks <aksmlibts@gmail.com>
2023-11-23 22:08:44 +0530

    inital commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,8 +6,7 @@ const Hero = () => {
   return (
     <div className='text-white'>
       <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-      <img src={esumImage} alt='Esum Image' className='w-full h-auto' />
-
+      <img src={esumImage} alt='Esum Image' className='w-100px h-auto' />
         <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>
           Enjoy The Summit.
         </h1>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,12 +10,12 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>E SUMMIT.</h1>
+      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>E SUMMIT 2023 @DSCE.</h1>
       <div onClick={handleNav} className='block md:hidden'>
           {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
       </div>
       <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>E SUMMIT.</h1>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>E SUMMIT 2023 @DSCE</h1>
       </ul>
     </div>
   );
