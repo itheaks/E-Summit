0a34bc634f0aba8b9ce1f2d869b88de143174a24
aks <aksmlibts@gmail.com>
2023-11-23 22:33:48 +0530

    inital commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import esumImage from './../assets/esum.jpg';
 const Hero = () => {
   return (
     <div className='text-white'>
-      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+      <div className='max-w-[500px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
       <img src={esumImage} alt='Esum Image' className='w-40% h-auto' />
         <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>
           Enjoy The Summit.
